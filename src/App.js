9809c195dc31a00f3ff9fbda5952bae2bc1f8be7
Chris York <christopherayork1992@gmail.com>
2019-07-22 14:35:09 -0400

    MVP done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+
+    <Route path='/' exact />
+    <Route  path='/about' />
+    <Route  path='/contact' />
+
   </div>
 );
 
